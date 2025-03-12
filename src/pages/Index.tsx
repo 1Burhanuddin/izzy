3409c3f3b99fd9235004e0021602c11ed7db3ab1
1Burhanuddin <118541489+1Burhanuddin@users.noreply.github.com>
2025-03-12 23:23:13 +0530

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -11,7 +11,7 @@ const Index = () => {
     <Layout>
       {/* Hero Section with neutral colors and cloud effect */}
       <section className="">
-        <div className="">
+        <div className="absolute inset-0 z-0">
           {/* Hero background image */}
           <img 
             src="https://images.unsplash.com/photo-1536532184021-da5392b55da1?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2340&q=80" 
