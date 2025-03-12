0c34014d7bc7730dd03896270a7d15d6f67297a0
1Burhanuddin <118541489+1Burhanuddin@users.noreply.github.com>
2025-03-12 23:20:28 +0530

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -16,7 +16,7 @@ const Index = () => {
           <img 
             src="https://images.unsplash.com/photo-1536532184021-da5392b55da1?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2340&q=80" 
             alt="Sky with clouds" 
-            className="w-full h-full "
+            className="w-full h-full object-cover opacity-40"
           />
           
           {/* Cloud-like overlay effect */}
