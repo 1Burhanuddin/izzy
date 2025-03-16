f83b90d6a271371fe4817967a7ca4437fb9c5c73
1Burhanuddin <118541489+1Burhanuddin@users.noreply.github.com>
2025-03-16 14:56:07 +0530

    Update Layout.tsx

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -29,7 +29,7 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
-      <main className="flex-grow pt-20">
+      <main className="flex-grow pt-10">
         {children}
         {isHomePage && (
           // <div className="container mx-auto px-4 py-8">
